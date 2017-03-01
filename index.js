f0c6ce21df2547d0b5a09046b77638383a1380eb
Shazron Abdullah <shazron@gmail.com>
2017-03-01 14:39:39 -0800

    Fixes #20 - Duplicates are shown. Fixed formatting

--- index.js
+++ index.js
@@ -5,39 +5,39 @@ var util = require('util');
 var rp = require('request-promise');
 var xml2js = require('xml2js');
 var Promise = require('bluebird');
-require ('newrelic');
+require('newrelic');
 
 var controller = Botkit.slackbot({
-  debug: false
+    debug: false
 });
 
 // connect the bot to a stream of messages
 var bot = controller.spawn({
-  token: process.env.SLACK_TOKEN,
+    token: process.env.SLACK_TOKEN
 });
 
 function startRTM() {
-	// start Slack RTM
-	bot.startRTM(function(err,bot,payload) {
-	    if (err) {
-			console.log('Failed to start RTM for bot1')
-			return setTimeout(startRtm, 60000);
-	    }
-	    console.log("RTM started for bot1!");
-	});
+    // start Slack RTM
+    bot.startRTM(function(err, bot, payload) {
+        if (err) {
+            console.log('Failed to start RTM for bot1')
+            return setTimeout(startRtm, 60000);
+        }
+        console.log("RTM started for bot1!");
+    });
 
-	if (process.env.SLACK_TOKEN_2) {
-		var bot2 = controller.spawn({
-  			token: process.env.SLACK_TOKEN_2,
-		});
-		bot2.startRTM(function(err,bot,payload) {
-			if (err) {
-				console.log('Failed to start RTM for bot2')
-				return setTimeout(startRtm, 60000);
-			}
-			console.log("RTM started for bot2!");
-		});		
-	}
+    if (process.env.SLACK_TOKEN_2) {
+        var bot2 = controller.spawn({
+            token: process.env.SLACK_TOKEN_2,
+        });
+        bot2.startRTM(function(err, bot, payload) {
+            if (err) {
+                console.log('Failed to start RTM for bot2')
+                return setTimeout(startRtm, 60000);
+            }
+            console.log("RTM started for bot2!");
+        });
+    }
 }
 
 controller.on('rtm_close', function(bot, err) {
@@ -54,73 +54,85 @@ controller.setupWebserver(process.env.PORT || 3001, function(err, webserver) {
 });
 
 // give the bot something to listen for.
-controller.hears('hello','direct_message,direct_mention,mention',function(bot,message) {
-  bot.reply(message,'Hello yourself.');
+controller.hears('hello', 'direct_message,direct_mention,mention', function(bot, message) {
+    bot.reply(message, 'Hello yourself.');
 });
 
 // give the bot something to listen for.
-controller.hears('help','direct_message,direct_mention,mention',function(bot,message) {
-  bot.reply(message,'Hello! I will expand any Cordova JIRA links in the form of CB-XXXX. Invite me to a channel, mention me in a message, or directly message me to see it in action.');
+controller.hears('help', 'direct_message,direct_mention,mention', function(bot, message) {
+    bot.reply(message, 'Hello! I will expand any Cordova JIRA links in the form of CB-XXXX. Invite me to a channel, mention me in a message, or directly message me to see it in action.');
 });
 
 
 // give the bot something to listen for.
-controller.hears('who are you','direct_message,direct_mention,mention',function(bot,message) {
+controller.hears('who are you', 'direct_message,direct_mention,mention', function(bot, message) {
 
-	bot.identifyBot(function(err,identity) {
-	  // identity contains...
-	  // {name, id, team_id}
-	  bot.reply(message, util.format("I'm %s.", identity.name));
-	})
+    bot.identifyBot(function(err, identity) {
+        // identity contains...
+        // {name, id, team_id}
+        bot.reply(message, util.format("I'm %s.", identity.name));
+    })
 });
 
 function newJiraXmlPromise(key) {
-	return new Promise(function(resolve, reject) {
-		key = key.toUpperCase();
-		var link = util.format('<https://issues.apache.org/jira/browse/%s|%s>', key, key);
-		rp(util.format('https://issues.apache.org/jira/si/jira.issueviews:issue-xml/%s/%s.xml', key, key))
-		   .then(function (xmlString) {
-			  xml2js.parseString(xmlString, function (err, result) {
-				  if (err) {
-					  var title = util.format('[%s] %s', key, '*error: issue key not found in JIRA*');
-					  resolve(title);					  
-				  } else {
-					  var summary = result.rss.channel[0].item[0].summary[0];
-					  var status = result.rss.channel[0].item[0].status[0]._;
-					  var statusIconUrl = result.rss.channel[0].item[0].status[0].$.iconUrl;
-					  var resolution = result.rss.channel[0].item[0].resolution[0]._;
-					  var title = util.format('[%s, (%s, %s)] %s', link, status, resolution,summary);
-					  resolve(title);
-				  }
-			  });
-		   })
-		   .catch(function (err) {
-			var title = util.format('[%s] %s', key, '*error: issue key not found in JIRA*');
-			resolve(title);					  
-		   });
-	}); 	
+    return new Promise(function(resolve, reject) {
+        key = key.toUpperCase();
+        rp(util.format('https://issues.apache.org/jira/si/jira.issueviews:issue-xml/%s/%s.xml', key, key))
+            .then(function(xmlString) {
+                xml2js.parseString(xmlString, function(err, result) {
+                    if (err) {
+                        var obj = { key: key, error: err };
+                        resolve(obj);
+                    } else {
+                        var obj = {
+                            key: key,
+                            summary: result.rss.channel[0].item[0].summary[0],
+                            status: result.rss.channel[0].item[0].status[0]._,
+                            statusIconUrl: result.rss.channel[0].item[0].status[0].$.iconUrl,
+                            resolution: result.rss.channel[0].item[0].resolution[0]._
+                        };
+
+                        resolve(obj);
+                    }
+                });
+            })
+            .catch(function(err) {
+                var obj = { key: key, error: err };
+                resolve(obj);
+            });
+    });
 }
 
-controller.hears('CB-[0-9]+',['direct_message','direct_mention','mention','ambient'],function(bot,message) {
+controller.hears('CB-[0-9]+', ['direct_message', 'direct_mention', 'mention', 'ambient'], function(bot, message) {
 
-	var re = /(CB-[0-9]+)/gi;
-	var result = message.text.match(re);
-	
-	if (result) {
-		var promises = [];
-  
-		result.forEach(function(key){
-		  promises.push(newJiraXmlPromise(key));
-		});
+    var re = /(CB-[0-9]+)/gi;
+    var result = message.text.match(re);
 
-	  	Promise.all(promises).then(function(values) {
-			bot.reply(message,{
-				text: values.join('\n'),
-				username: "CordovaHelpBot",
-				icon_emoji: ":gear:",
-			}); 
-	  	});
-	}
+    if (result) {
+        var promises = result
+            .filter(function(elem, index, self) {  // remove duplicate keys
+                return index == self.indexOf(elem); 
+            })
+            .map(function(elem) { // transform each element to a promise
+                return newJiraXmlPromise(elem);
+            });
+        
+        Promise.all(promises).then(function(values) {
+            values = values.map(function(val) { // transform the objects to strings
+                if (!val.err) {
+                    var link = util.format('<https://issues.apache.org/jira/browse/%s|%s>', val.key, val.key);
+                    return util.format('[%s, (%s, %s)] %s', link, val.status, val.resolution, val.summary);
+                } else {
+                    return util.format('[%s] %s', val.key, '*error: issue key not found in JIRA*')
+                }
+            });
+            bot.reply(message, {
+                text: values.join('\n'),
+                username: "CordovaHelpBot",
+                icon_emoji: ":gear:",
+            });
+        });
+    }
 });
 
 // JIRA REST API, here "orientation" is being searched for. paged at 5, starting at 0. only showing link and summary fields
